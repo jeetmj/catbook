5ebaf60f151fb89ebbf41c9024bd525b7aad5827
Rupayan Neogy <rneogy@mit.edu>
2018-01-05 13:28:07 -0500

    added more to profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -9,14 +9,27 @@ async function main() {
 }
 
 function renderUserData(user) {
-	const profileContainer = document.getElementById('name-container');
-
+	// rendering name
+	const nameContainer = document.getElementById('name-container');
 	const nameHeader = document.createElement('h1');
 	nameHeader.innerHTML = user.name;
-	profileContainer.appendChild(nameHeader);
+	nameContainer.appendChild(nameHeader);
 
+	// rendering profile image
 	const profileImage = document.getElementById('profile-image');
 	profileImage.style = 'background-image:url(https://i.pinimg.com/736x/98/e0/7d/98e07decc7c1ca58236995de3567e46a--cat-shirts-kitties-cutest.jpg)';
+
+	// rendering latest post
+	const latestPostCard = document.getElementById('latest-post-card');
+  	const creatorSpan = document.createElement('a');
+  	creatorSpan.className = 'story-creator card-title';
+  	creatorSpan.innerHTML = user.name;
+  	creatorSpan.setAttribute('href', '/u/profile?'+user._id);
+  	latestPostCard.appendChild(creatorSpan);
+	const latestPost = document.createElement('p');
+	latestPost.className = 'story-content card-text';
+  	latestPost.innerHTML = user.last_post;
+  	latestPostCard.appendChild(latestPost);
 }
 
 main();
